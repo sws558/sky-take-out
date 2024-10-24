7403bc825f008d16e1920a44b8b1d3eb25e31c69
sws <1263654270@qq.com>
2024-10-24 20:47:17 +0800

    新增员工业务代码开发

--- sky-common/src/main/java/com/sky/constant/MessageConstant.java
+++ sky-common/src/main/java/com/sky/constant/MessageConstant.java
@@ -4,7 +4,7 @@ package com.sky.constant;
  * 信息提示常量类
  */
 public class MessageConstant {
-
+    
     public static final String PASSWORD_ERROR = "密码错误";
     public static final String ACCOUNT_NOT_FOUND = "账号不存在";
     public static final String ACCOUNT_LOCKED = "账号被锁定";
@@ -24,4 +24,5 @@ public class MessageConstant {
     public static final String ORDER_STATUS_ERROR = "订单状态错误";
     public static final String ORDER_NOT_FOUND = "订单不存在";
 
+    public static final String ALREADY_EXISTS = "账号已存在";
 }
